843e7212694cf7174876c8789b832a77069f62d5
Jeongseup <seup87@gmail.com>
2022-04-18 23:41:52 +0900

    sh study - 2. read commands

--- 2_read-user-input.sh
+++ 2_read-user-input.sh
@@ -1,5 +1,24 @@
 #! /bin/bash
 
+# read user inputs 
+echo "Enter names : "
+read n1 n2 n3
+echo "Entered names : $n1 , $n2, $n3"
+
+# read user input with print msg
+read -p "username : " user_var
+read -sp "password : " pass_var # hide user input, but save 
+echo
+echo "username : $user_var"
+echo "pw : $pass_var"
+
+# read user input by using array
+echo "Enter names arr : "
+read -a names
+echo "Name arr :  ${names[0]}, ${names[1]} ..."
+echo "All names are ${names[@]}"
+
 echo "Enter name : "
-read name
-echo "Entered name : $name"
+read
+echo "Entered var : $REPLY"
+
