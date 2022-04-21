85ab6a2b9ce2724291666b64fa99095100bb8169
Jeongseup <seup87@gmail.com>
2022-04-21 10:28:34 +0900

    sh study - 13. case statement with regular expression

--- 13_case-statment2.sh
+++ 13_case-statment2.sh
@@ -1,24 +1,19 @@
 #! /bin/bash
 
-# receive your input
-vehicle=$1
+echo -e "Enter some character : \c"
+read value
 
-# case statment like switch in general programming lang
-case $vehicle in 
-    # pattern1 )
-    #     statements ;;
-
-    # pattern2 )
-    #     statements ;;
-    # ...
-
-    "car" ) 
-        echo "Rent of $vehicle 100 dollar" ;;
-
-    "van" ) 
-        echo "Rent of $vehicle 100 dollar" ;;
-
+case $value in 
+    # regular expression
+    [a-z] ) 
+        echo "User entered $value in from a to z" ;;
+    [A-Z] ) 
+        echo "User entered $value in from A to Z" ;;    
+    [0-9] )
+        echo "User entered $value in from 0 to 9" ;;
+    ? )
+        echo "User entered $value in special character" ;;
     * )
-        echo "Unknown vehicle" ;;
+        echo "Unknown" ;;
 
 esac
